e51acd8f525c930e9c2c22b37529de0c6ad5bb43
Nicolas Alfaro <79803738+Chaiinito@users.noreply.github.com>
2021-11-27 21:05:36 -0600

    Update sketch_nov25a.ino

--- sketch_nov25a.ino
+++ sketch_nov25a.ino
@@ -25,7 +25,7 @@ int in4 = 9;
 int ENA = 10;
 int ENB = 5;
 
-//bool delante;
+bool delante;
 
 /*
 unsigned long tiempo = 0;
@@ -66,7 +66,6 @@ void loop() {
 
 
 void PID_control(){
-  bool delante = false;
 
   int i;
   uint16_t blocks;
@@ -77,65 +76,40 @@ void PID_control(){
       uint16_t ancho = pixy.blocks[i].width;
       uint16_t alto = pixy.blocks[i].height;
       area = ancho * alto;
-      //pixy.blocks[i].print();
-      //Serial.print(area);
+      
       Serial.print('\n');
       error = 11000 - area;     // referencia a 20 cm son 11000 del area
       Serial.print(error);
-      if(error >= 0){
+      
+      P = error;
+      I = I + error;
+      D = error - lastError;
+      lastError = error;
+    
+      int motorspeed = P*Kp + I*Ki + D*Kd;
+    
+      int motorspeeda = basespeeda + motorspeed;
+      int motorspeedb = basespeedb - motorspeed;
+    
+      
+      if(error >= 1000){                                  // Si el objeto esta muy lejos, mover los motores a favor a las manecillas del reloj
         delante = true;
-      }
-      else if(error < 0){
+        break;
+        }
+      else if(error < -1000){                            // Si el objeto esta muy cerca, mover los motores en sentido contrario a las manecillas del reloj
         delante = false;
-      }
+        }
+      else if((error > -1000) && (error < 1000)){       // Zona muerta
+        foward_backwards(0, 0, delante); 
+        }
+      else if((error > 9000) && (error < 13000)){      //No activarse con falsos positivos
+        foward_backwards(0, 0, delante);
+        }
+        
+      foward_backwards(motorspeeda, motorspeedb, delante);
     }
   }
-  P = error;
-  I = I + error;
-  D = error - lastError;
-  lastError = error;
-
-  int motorspeed = P*Kp + I*Ki + D*Kd;
-
-  int motorspeeda = basespeeda + motorspeed;
-  int motorspeedb = basespeedb - motorspeed;
-//
-//
-//  if ((area >= 5000) && (area < 6000)){     //Si se encuentra dentro del rango no mover ni hacia adelante ni hacia atras
-//    direccion = false;
-//    lados = false;
-//    motorspeeda = 0;
-//    motorspeedb = 0;
-//    
-//  }
-//  else if((area < 5000)&&(area >= 500)){
-//    direccion = true; 
-//    lados = false;                //Mover hacia delante
-//    motorspeeda = maxspeeda;
-//    motorspeedb = maxspeedb;
-//    
-//  }
-//  else if(area >= 6000){            //Mover hacia atras
-//    direccion = false;
-//    lados = false;
-//    motorspeeda = maxspeeda;
-//    motorspeedb = maxspeedb;
-//    
-//  }
-//  else if(area < 100){                         //Cuando no detecte el objeto mover hacia los lados
-//    lados = true;
-//    motorspeeda = 0;
-//    motorspeedb = 0;
-//  }
-//  
-//  //else{
-// //   motorspeeda = 0;
-//  motorspeedb = 0;
-//}
-
-  foward_backwards(motorspeeda, motorspeedb, delante);
 }
-
 void foward_backwards(int posa, int posb, bool direccion){
   //tiempo = millis();
 
