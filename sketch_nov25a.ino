0ae6318f19e92a1d8ffaa8d24fead6bd21915b8e
Nicolas Alfaro <79803738+Chaiinito@users.noreply.github.com>
2021-11-27 20:23:49 -0600

    Update sketch_nov25a.ino

--- sketch_nov25a.ino
+++ sketch_nov25a.ino
@@ -16,6 +16,7 @@ int lastError = 0;
 int ancho;
 int alto;
 int long area;
+signed long int error;
 
 int in1 = 6;
 int in2 = 7;
@@ -24,15 +25,17 @@ int in4 = 9;
 int ENA = 10;
 int ENB = 5;
 
+//bool delante;
 
+/*
 unsigned long tiempo = 0;
 const long inicio = 2000;
 const long tiempo_1 = 3000;
 const long tiempo_2 = 4000;
 const long tiempo_3 = 5000;
-
-const uint8_t maxspeeda = 100;
-const uint8_t maxspeedb = 110;
+*/
+const uint8_t maxspeeda = 150;
+const uint8_t maxspeedb = 150;
 const uint8_t basespeeda = 100;
 const uint8_t basespeedb = 100;
 
@@ -51,51 +54,20 @@ void setup() {
 
 void loop() {
   PID_control();
-
-
-//void bloque(){
-//  static int i = 0;
-//  int j;
-//  uint16_t blocks;
-//  blocks = pixy.getBlocks();
-//  char buf[32]; 
-//  if (blocks)
-//  {
-//    i++;
-//    if (i%6==0) //50 -> 1 dps   25 -> 2dps   13-> 4dps   6 -> 8dps 
-//    {
-//      for (j=0; j<blocks; j++)
-//      {
-//        Serial.print(buf); 
-//        pixy.blocks[j].print();
-//      }
-//    }
-//  }
-//}
-  
-}
-void foward_backwards(int posa, int posb, bool dir){
-  tiempo = millis();
-
-  if(dir == true){                // Si dir mover hacia adelante
-    analogWrite(ENA,posa);
-    analogWrite(ENB,posb);
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2,LOW);
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4,LOW);
+  /*
+  if(area < 200){
+    foward_backwards (0,0, false, false);
   }
-  else if(dir == false){           // else mover hacia atras
-    analogWrite(ENA,posa);
-    analogWrite(ENB,posb);
-    digitalWrite(in1, LOW);
-    digitalWrite(in2,HIGH);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4,HIGH);
+  else{
+    PID_control();
   }
+  */
 }
+
 
 void PID_control(){
+  bool delante = false;
+
   int i;
   uint16_t blocks;
   blocks = pixy.getBlocks();
@@ -104,18 +76,20 @@ void PID_control(){
     for(i=0; i<=blocks;i++){
       uint16_t ancho = pixy.blocks[i].width;
       uint16_t alto = pixy.blocks[i].height;
-      uint16_t posX = pixy.blocks[i]. ; 
-      
       area = ancho * alto;
-      pixy.blocks[i].print();
+      //pixy.blocks[i].print();
+      //Serial.print(area);
+      Serial.print('\n');
+      error = 11000 - area;     // referencia a 20 cm son 11000 del area
+      Serial.print(error);
+      if(error >= 0){
+        delante = true;
+      }
+      else if(error < 0){
+        delante = false;
+      }
     }
   }
-  
-  int error = 5500 - area;
-  
-  bool direccion;
-  bool lados;
-
   P = error;
   I = I + error;
   D = error - lastError;
@@ -125,53 +99,68 @@ void PID_control(){
 
   int motorspeeda = basespeeda + motorspeed;
   int motorspeedb = basespeedb - motorspeed;
-  if (motorspeeda > maxspeeda) {
-    motorspeeda = maxspeeda;
-  }
-  if (motorspeedb > maxspeedb) {
-    motorspeedb = maxspeedb;
-  }
-  if (motorspeeda < 0) {
-    motorspeeda = 0;
-  }
-  if (motorspeedb < 0) {
-    motorspeedb = 0;
-  } 
+//
+//
+//  if ((area >= 5000) && (area < 6000)){     //Si se encuentra dentro del rango no mover ni hacia adelante ni hacia atras
+//    direccion = false;
+//    lados = false;
+//    motorspeeda = 0;
+//    motorspeedb = 0;
+//    
+//  }
+//  else if((area < 5000)&&(area >= 500)){
+//    direccion = true; 
+//    lados = false;                //Mover hacia delante
+//    motorspeeda = maxspeeda;
+//    motorspeedb = maxspeedb;
+//    
+//  }
+//  else if(area >= 6000){            //Mover hacia atras
+//    direccion = false;
+//    lados = false;
+//    motorspeeda = maxspeeda;
+//    motorspeedb = maxspeedb;
+//    
+//  }
+//  else if(area < 100){                         //Cuando no detecte el objeto mover hacia los lados
+//    lados = true;
+//    motorspeeda = 0;
+//    motorspeedb = 0;
+//  }
+//  
+//  //else{
+// //   motorspeeda = 0;
+//  motorspeedb = 0;
+//}
 
+  foward_backwards(motorspeeda, motorspeedb, delante);
+}
 
-  /*if ((area >= 5000) && (area < 6000)){     //Si se encuentra dentro del rango no mover ni hacia adelante ni hacia atras
-    direccion = false;
-    lados = false;
-    motorspeeda = 0;
-    motorspeedb = 0;
-    
+void foward_backwards(int posa, int posb, bool direccion){
+  //tiempo = millis();
+
+  if(direccion == true){                // Si dir mover hacia adelante
+    analogWrite(ENA,posa);
+    analogWrite(ENB,posb);
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2,LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4,LOW);
   }
-  else if((area < 5000)&&(area >= 1000)){
-    direccion = true; 
-    lados = false;                //Mover hacia delante
-    motorspeeda = maxspeeda;
-    motorspeedb = maxspeedb;
-    
-  }
-  else if(area >= 6000){            //Mover hacia atras
-    direccion = false;
-    lados = false;
-    motorspeeda = maxspeeda;
-    motorspeedb = maxspeedb;
-    
-  }
-  else if(area < 1000){                         //Cuando no detecte el objeto mover hacia los lados
-    lados = true;
-    motorspeeda = 0;
-    motorspeedb = 0;
-  }
-  
-  else{
-    motorspeeda = 0;
-    motorspeedb = 0;
+  else if(direccion == false){                   // else mover hacia atras
+    analogWrite(ENA,posa);
+    analogWrite(ENB,posb);
+    digitalWrite(in1, LOW);
+    digitalWrite(in2,HIGH);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4,HIGH);
+  }/*
+  if (lado == 1){
+    analogWrite(ENA,posa);
+    analogWrite(ENB,posb);
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2,LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4,HIGH);
   }*/
-  
-  Serial.print(area);
-  
-  foward_backwards(motorspeeda, motorspeedb, direccion, lados);
 }
